e7c5eee03c30b1836fd67eeaeac91a0067d5a5a2
IntenceYT <lasse01@protonmail.com>
2018-05-02 19:20:37 +0200

    Change loading screen title

--- minecraft/net/minecraft/client/Minecraft.java
+++ minecraft/net/minecraft/client/Minecraft.java
@@ -690,7 +690,7 @@ public class Minecraft implements IThreadListener, ISnooperInfo
     private void createDisplay() throws LWJGLException
     {
         Display.setResizable(true);
-        Display.setTitle("Minecraft 1.12");
+        Display.setTitle(Client.NAME + " loading...");
 
         try
         {
